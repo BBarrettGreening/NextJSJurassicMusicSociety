819284ea6a55243eba826cf15380c3a2ebdc8261
Bustamante Barrett-Greening <146181717+BBarrettGreening@users.noreply.github.com>
2024-10-07 13:59:55 +0100

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || '<https://bustamantebg.uk>',
+    generateRobotsTxt: true, // optional
+    // other configurations...
+  }
