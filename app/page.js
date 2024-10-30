f4bc28d7296304c66c55dcf857a0881890e47e8d
Bustamante Barrett-Greening <146181717+BBarrettGreening@users.noreply.github.com>
2024-10-30 12:04:18 +0000

    moved analytic tag

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import HomeSection from "./components/HomeSection";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-white">
       <SpeedInsights />
+      <Analytics />
       <Navbar />
       <div className="mt-24 container mx-auto px-12 min-h-screen">
         <main className="flex-grow">
