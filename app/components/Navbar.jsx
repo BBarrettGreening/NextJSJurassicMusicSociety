524b184c1b1a245ac53cbd1a8acfd3d2bfa93f21
Bustamante Barrett-Greening <146181717+BBarrettGreening@users.noreply.github.com>
2024-10-30 17:49:27 +0000

    header menu button accessibility improvements

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -29,7 +29,7 @@ const navLinks = [
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
-    <nav className="fixed top-0 left-0 right-0 z-5 bg-[#252525] bg-opacity-100">
+    <nav className="fixed h-fill top-0 left-0 right-0 z-5 bg-[#252525] bg-opacity-100">
         <div className="flex flex-wrap md:flex-row items-center justify-between mx-auto px-4">
             <Link href="/" className="text-3xl text-white font-semibold">
             Jurassic Music Society
@@ -38,11 +38,11 @@ const Navbar = () => {
             {
                 !navbarOpen ? (
                     <button onClick={ () => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white">
-                    <Bars3Icon className="h-5 w-5" />
+                    <Bars3Icon className="h-20 w-20" />
                     </button>
                 ) : (
                     <button onClick={ () => setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white">
-                    <XMarkIcon className="h-5 w-5" />
+                    <XMarkIcon className="h-20 w-20" />
                     </button>
                 )
             }
