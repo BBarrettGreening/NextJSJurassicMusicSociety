fb83c66821c09a0901bc9cbbe52ba2139b9365ab
Bustamante Barrett-Greening <146181717+BBarrettGreening@users.noreply.github.com>
2024-10-31 17:01:59 +0000

    title and menu icon display correctly mobile

--- app/components/HomeSection.jsx
+++ app/components/HomeSection.jsx
@@ -6,9 +6,9 @@ import { TypeAnimation } from 'react-type-animation';
 const HomeSection = () => {
   return (
     <section>
-        <div className="md:mt-20 grid grid-cols-1 lg:gap-16 sm:grid-cols-12">
+        <div className="md:mt-20 sm:mt-5 grid grid-cols-1 lg:gap-16 sm:grid-cols-12">
             <div className="col-span-7 place-self-center mb-6 text-center sm:text-left">
-                <p className="text-black text-base mb-4 sm:text-lg lg:text-2xl">
+                <p className="text-black text-base mb-4 sm:text-lg lg:text-2xl sm:mt-5">
                 Welcome to the Jurassic Music Society, your community-driven radio destination! Hosted by Rich, Jake, and Harry, we bring your favorite tunes directly to your speakers. Whether it&apos;s Tecno, drum and bass, rock, jazz, acoustic, classical, or the latest hits, we play it all. Dive into diverse genres and explore music from every corner of the globe with us. We thrive on your requests and reviews, making sure each broadcast is uniquely tailored to our listeners. Tune in, join the conversation, and let Rich, Jake, and Harry play your music, for you!
                 </p>
                 <div>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -29,38 +29,40 @@ const navLinks = [
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
-    <nav className="fixed h-fill top-0 left-0 right-0 z-5 bg-[#252525] bg-opacity-100">
-        <div className="flex flex-wrap md:flex-row items-center justify-between mx-auto px-4">
-            <Link href="/" className="text-3xl text-white font-semibold">
-            Jurassic Music Society
-            </Link>
-            <div className="mobile-menu block lg:hidden">
-            {
-                !navbarOpen ? (
-                    <button onClick={ () => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white">
-                    <Bars3Icon className="h-20 w-20" />
-                    </button>
-                ) : (
-                    <button onClick={ () => setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white">
-                    <XMarkIcon className="h-20 w-20" />
-                    </button>
-                )
-            }
-
-            </div>
-            <div className="menu hidden lg:block lg:w-auto" id="navbar">
-                <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mr-0 mt-5">
-                    {navLinks.map((link, index) => (
+        <nav className="fixed h-fill top-0 left-0 right-0 z-5 bg-[#252525] bg-opacity-100">
+            <div className="flex flex-wrap md:flex-row items-center justify-between mx-auto px-4">
+                <div className="flex items-center justify-between w-full lg:w-auto">
+                    <Link href="/" className="md:text-3xl sm:text-2xl text-white font-semibold">
+                        Jurassic Music Society
+                    </Link>
+                    <div className="mobile-menu block lg:hidden">
+                        {
+                            !navbarOpen ? (
+                                <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white">
+                                    <Bars3Icon className="h-20 w-20" />
+                                </button>
+                            ) : (
+                                <button onClick={() => setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white">
+                                    <XMarkIcon className="h-20 w-20" />
+                                </button>
+                            )
+                        }
+                    </div>
+                </div>
+                <div className="menu hidden lg:block lg:w-auto" id="navbar">
+                    <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mr-0 mt-5">
+                        {navLinks.map((link, index) => (
                             <li key={index}>
                                 <NavLink href={link.path} title={link.title} />
-                            </li> 
+                            </li>
                         ))}
-                </ul>
+                    </ul>
+                </div>
             </div>
-        </div>
-        {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
-    </nav>
+            {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+        </nav>
     )
 }
+
 
 export default Navbar
